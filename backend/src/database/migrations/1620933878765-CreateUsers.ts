89876d3241381e359f90e26b49abf37c214f6c24
Diego Ferreira <33259326+di-ferreira@users.noreply.github.com>
2021-05-13 16:57:11 -0300

    TypeOrm migrationx

---
+++ backend/src/database/migrations/1620933878765-CreateUsers.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1620933878765 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
