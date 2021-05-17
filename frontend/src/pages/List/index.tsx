b52dcb492e98160aa3bcc20ccef1e914e56693f3
Diego Ferreira <diferreira.program@gmail.com>
2021-05-16 22:12:13 -0300

    list page

--- frontend/src/pages/List/index.tsx
+++ frontend/src/pages/List/index.tsx
@@ -3,39 +3,68 @@ import ProfileImg from "../../assets/img/hero.png";
 
 function List() {
   return (
-    <div className="justify-content-center d-flex w-100 h100 bg-login">
-      <div className="d-flex flex-column card-login align-items-center">
-        <h1 className="mt-4 title-custom">DNA User List</h1>
-        <div className="px-5 mt-4 align-items-end d-flex flex-column w-90">
-          <ul className="user-list-container">
-            <li className="user-list-item">
-              <Link to="/profile">
+    <div className="justify-content-center d-flex w-100 h100">
+      <div className="d-flex flex-column card-register">
+        <h1 className="mt-3 title-custom">DNA User List</h1>
+        <ul className="user-list-container mt-5">
+          <li className="user-list-item w-65">
+            <Link
+              to="/profile"
+              className="w-100 h-100 d-flex justify-content-end align-items-center"
+            >
+              <div className="user-list-item-container-image">
                 <img src={ProfileImg} alt="" className="user-list-item-image" />
-                <h3 className="user-list-item-name">Aquiles Vieira Ferreira</h3>
-                <h5 className="user-list-item-skills">UI/UX & Web Developer</h5>
-                <span className="user-list-item-status-01">Freelancer</span>
-              </Link>
-            </li>
-            <li className="user-list-item">
-              <Link to="/profile">
-                <img src={ProfileImg} alt="" className="user-list-item-image" />
+              </div>
+              <div className="user-list-item-info">
                 <h3 className="user-list-item-name">
                   Diego dos Santos Ferreira
                 </h3>
-                <h5 className="user-list-item-skills">Front Edn Developer</h5>
-                <span className="user-list-item-status-02">Employed</span>
-              </Link>
-            </li>
-            <li className="user-list-item">
-              <Link to="/profile">
+                <p className="user-list-item-skills">Front End Developer</p>
+              </div>
+              <span className="user-list-item-status" data-employed>
+                Employed
+              </span>
+            </Link>
+          </li>
+          <li className="user-list-item w-65">
+            <Link
+              to="/profile"
+              className="w-100 h-100 d-flex justify-content-end align-items-center"
+            >
+              <div className="user-list-item-container-image">
                 <img src={ProfileImg} alt="" className="user-list-item-image" />
-                <h3 className="user-list-item-name">Priscila Gomes Vieiera</h3>
-                <h5 className="user-list-item-skills">Back End</h5>
-                <span className="user-list-item-status-03">Unemployed</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+              </div>
+              <div className="user-list-item-info">
+                <h3 className="user-list-item-name">
+                  Diego dos Santos Ferreira
+                </h3>
+                <p className="user-list-item-skills">Front End Developer</p>
+              </div>
+              <span className="user-list-item-status" data-unemployed>
+                Employed
+              </span>
+            </Link>
+          </li>
+          <li className="user-list-item w-65">
+            <Link
+              to="/profile"
+              className="w-100 h-100 d-flex justify-content-end align-items-center"
+            >
+              <div className="user-list-item-container-image">
+                <img src={ProfileImg} alt="" className="user-list-item-image" />
+              </div>
+              <div className="user-list-item-info">
+                <h3 className="user-list-item-name">
+                  Diego dos Santos Ferreira
+                </h3>
+                <p className="user-list-item-skills">Front End Developer</p>
+              </div>
+              <span className="user-list-item-status" data-freelancer>
+                Employed
+              </span>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
