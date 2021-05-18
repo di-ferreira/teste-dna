a1a609dfe0312a863441635366185d26ebaa51fe
Diego Ferreira <diferreira.program@gmail.com>
2021-05-18 20:31:35 -0300

    Finaly page profile

--- frontend/src/pages/List/index.tsx
+++ frontend/src/pages/List/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
 
-
 function List() {
   const [users, setUsers] = useState<any[]>();
 
@@ -13,38 +12,35 @@ function List() {
       api.defaults.headers.common["Authorization"] = "Bearer " + token;
     }
 
-    api.get(`/users/`).then((response: { data: any; }) => {
+    api.get(`/users/`).then((response: { data: any }) => {
       const usersData = response.data;
       setUsers(usersData);
     });
-
-  }, [])
+  }, []);
   return (
     <div className="justify-content-center d-flex w-100 h100">
       <div className="d-flex flex-column card-register">
         <h1 className="mt-3 title-custom">DNA User List</h1>
         <ul className="user-list-container mt-5">
-          {
-            users?.map((u) => (
-              < li className="user-list-item w-65" key={u.id} >
-                <Link
-                  to={`/profile/${u.id}`}
-                  className="w-100 h-100 d-flex justify-content-end align-items-center"
-                >
-                  <div className="user-list-item-info d-flex justify-content-end align-items-center flex-column">
-                    <h3 className="user-list-item-name">
-                      {u.user.name}
-                    </h3>
-                    <p className="user-list-item-skills">Front End Developer</p>
-                    <span className="user-list-item-status" data-status={u.status}>
-                      {u.status}
-                    </span>
-                  </div>
-                </Link>
-              </li>
-
-            ))
-          }
+          {users?.map((u) => (
+            <li className="user-list-item w-65" key={u.id}>
+              <Link
+                to={`/profile/${u.id}`}
+                className="w-100 h-100 d-flex justify-content-end align-items-center"
+              >
+                <div className="user-list-item-info d-flex justify-content-end align-items-center flex-column">
+                  <h3 className="user-list-item-name">{u.user.name}</h3>
+                  <p className="user-list-item-skills">Front End Developer</p>
+                  <span
+                    className="user-list-item-status"
+                    data-status={u.status.toLowerCase()}
+                  >
+                    {u.status.toUpperCase()}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,14 +1,40 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import api from "../../services/api";
+import { formatLocalDate } from "../../utils";
+
+interface IdParam {
+  id: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  birthdate: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: string;
+  status: string;
+}
 
 function Profile() {
   //captura id do usuario
-  const id = useParams();
+  const dataParam = useParams<IdParam>();
+  const id = dataParam.id;
 
-  const [user, setUser] = useState<any[]>();
+  const [user, setUser] = useState<User>({
+    id: "",
+    name: "",
+    birthdate: "",
+    email: "",
+    phone: "",
+    website: "",
+    address: "",
+    status: "",
+  });
 
   useEffect(() => {
     const token = localStorage.getItem("@DNA:token");
@@ -17,21 +43,32 @@ function Profile() {
       api.defaults.headers.common["Authorization"] = "Bearer " + token;
     }
 
-    api.get(`/users/${id}`).then((response: { data: any; }) => {
-      const usersData = response.data;
+    api.get(`/users/${id}`).then((response: { data: any }) => {
+      const data = response.data;
+      const status = data.status.toLowerCase();
+      const birthdate = formatLocalDate(data.birthdate, "dd/MM/yyyy");
+      const usersData = {
+        id: data.id,
+        name: data.user.name,
+        birthdate,
+        email: data.user.email,
+        phone: data.phone,
+        website: data.website,
+        address: data.address,
+        status,
+      };
 
-      console.log(response)
-      // setUser(usersData);
+      setUser(usersData);
     });
-
-  }, [])
-
-
+  }, [id]);
   return (
     <div className="justify-content-center align-items-center flex-column d-flex w-100 h100">
-      <div className="d-flex align-items-center  pb-1 card-profile-container">
+      <div
+        className="d-flex align-items-center  pb-1 card-profile-container"
+        // key={user.key}
+      >
         <div className="card-profile d-flex align-items-center justify-content-center">
-          <h1 className="title-custom mt-3">Diego Ferreira</h1>
+          <h1 className="title-custom mt-3">{user.name}</h1>
           <p>Front End Developer</p>
         </div>
         <div className="card-profile-info mt-4">
@@ -39,36 +76,45 @@ function Profile() {
           <ul className="card-profile-info-details">
             <li>
               <span>Birthdate</span>
-              21-02-1992
+              {user.birthdate}
             </li>
             <li>
               <span>Phone</span>
-              +91 99886644332
+              +55 {user.phone}
             </li>
             <li>
               <span>Email</span>
-              cv@example.com
+              {user.email}
             </li>
             <li>
-              <span> website</span>www.example.com
+              <span> website</span>
+              {user.website}
             </li>
             <li>
               <span>ADDRESS</span>
-              123 London, UK Job
+              {user.address}
             </li>
             <li>
               <span>Status</span>
-              <span className="card-profile-status" data-employed>
-                Employed
+              <span className="card-profile-status" data-status={user.status}>
+                {user.status.toUpperCase()}
               </span>
             </li>
           </ul>
         </div>
       </div>
-      <Link to="/" className="btn btn-custom-primary d-flex justify-content-center">
-        <FontAwesomeIcon icon={faSignInAlt} rotation={180} size="lg" className="mr-2" />
-          Go Back
-        </Link>
+      <Link
+        to="/"
+        className="btn btn-custom-primary d-flex justify-content-center"
+      >
+        <FontAwesomeIcon
+          icon={faSignInAlt}
+          rotation={180}
+          size="lg"
+          className="mr-2"
+        />
+        Go Back
+      </Link>
     </div>
   );
 }
